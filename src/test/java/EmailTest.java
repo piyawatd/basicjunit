2833ce842b5268c39e27a87f21aa4bf35624a4e0
piyawatd <arunwatd@gmail.com>
2020-06-16 11:41:21 +0700

    add check result true

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -16,45 +16,59 @@ class EmailTest {
     @Test
     void checkSpaceBefore() {
         assertFalse(email.checkSpaceBefore(" testemail@gmail.com"));
+        assertTrue(email.checkSpaceBefore("testemail@gmail.com"));
     }
 
     @Test
     void checkSpaceAfter() {
         assertFalse(email.checkSpaceAfter("testemail@gmail.com "));
+        assertTrue(email.checkSpaceAfter("testemail@gmail.com"));
     }
 
     @Test
     void checkSpace() {
         assertFalse(email.checkSpace(" testemail@gmail.com "));
+        assertTrue(email.checkSpace("testemail@gmail.com"));
     }
 
     @Test
     void checkBlank() {
         assertFalse(email.checkBlank(""));
+        assertTrue(email.checkBlank("testemail@gmail.com"));
     }
 
     @Test
     void checkNoAtSign() {
         assertFalse(email.checkNoAtSign(" testemail"));
+        assertTrue(email.checkNoAtSign("testemail@gmail.com"));
     }
 
     @Test
     void checkBlankBeforeAtSign() {
         assertFalse(email.checkBlankBeforeAtSign("@gmail.com"));
+        assertTrue(email.checkBlankBeforeAtSign("testemail@gmail.com"));
     }
 
     @Test
     void checkEmailName() {
         assertFalse(email.checkEmailName("testemail#@gmail.com"));
+        assertTrue(email.checkEmailName("testemail@gmail.com"));
     }
 
     @Test
     void checkDot() {
         assertFalse(email.checkDot("testemail@gmail"));
+        assertTrue(email.checkDot("testemail@gmail.com"));
     }
 
     @Test
     void checkUniqueEmail() {
         assertFalse(email.checkUniqueEmail("testemail@gmail.com"));
+        assertTrue(email.checkUniqueEmail("test@gmail.com"));
+    }
+
+    @Test
+    void checkEmailPass() {
+        assertTrue(email.checkEmailName("test@gmail.com"));
     }
 }
